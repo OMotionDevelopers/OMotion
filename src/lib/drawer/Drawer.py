4f1467400dfaf26756172b1e2f2741e972fdc968
Giovanni Cardamone <g.cardamone2@gmail.com>
2017-05-16 10:15:49 +0200

    up

--- src/lib/drawer/Drawer.py
+++ src/lib/drawer/Drawer.py
@@ -3,8 +3,20 @@ import cv2
 class Drawer:
 
 	@staticmethod
-	def draw(frame, polygon, color, size):
+	def drawPolygon(frame, polygon, color, size):
 		edges = polygon.edges()
 
 		for e in edges:
 			cv2.line(frame, e.p0.toCv(), e.p1.toCv(), color, size)
+
+
+	@staticmethod
+	def drawPolygons(frame, polygons, color, size):
+		for polygon in polygons:
+			Drawer.drawPloygon(frame, polygon, color, size)
+
+
+	@staticmethod
+	def drawContour(frame, contour, color, size):
+		cv2.drawContours(frame, [contour], 0, color, size)
+
